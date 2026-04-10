ccb73baae79b13a4ede55049cf178de1c1d6ca37
Artem <artem.ulianov@gmail.com>
2026-04-10 16:15:26 +0200

    save

--- src/show.py
+++ src/show.py
@@ -1,40 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.patches import Circle
 from reprojection import *
-
-
-def show_polar_data(data, figsize=(10,10), label='', **kwargs):
-    fig = plt.figure(figsize=figsize)
-
-    axes_coords = (0.05, 0.05, 0.9, 0.9)
-
-    ax_image = fig.add_axes(axes_coords)
-    im = ax_image.imshow(data, origin='lower', **kwargs)
-
-    ax_image.axis('off')
-
-    cax = ax_image.inset_axes((0.8, 0.985, 0.2, 0.015))
-    fig.colorbar(im, cax=cax, orientation='horizontal', label=label)
-
-    ax_polar = fig.add_axes(axes_coords, projection = 'polar')
-    ax_polar.patch.set_alpha(0)
-
-    lats = np.arange(15,91,15)
-
-    ax_polar.set_rorigin(0)
-    ax_polar.set_rticks(np.cos(lats * np.pi / 180))
-    ax_polar.set_rlabel_position(-90)
-    ax_polar.set_yticklabels([rf'{lat}$\degree$' for lat in lats], color='black')
-
-    ax_polar.grid(True, ls='--', color='black', alpha=0.6)
-
-    return fig, ax_image
 
 
 def show_data(data, view, title='', label='', figsize=(10,10), cmap='seismic', vmin=-200, vmax=200, to_file=None):
     transform = ~(view.to_carrington() + ToSpherical())
 
-    grid = np.mgrid[-90:90.5:1,-180:180.5:1]
+    grid = np.mgrid[-90:90.5:1,0:360.5:1]
     grid, alpha = transform(grid)
     grid = np.array(grid)
 
@@ -46,13 +19,17 @@ def show_data(data, view, title='', label='', figsize=(10,10), cmap='seismic', v
 
     fig, ax = plt.subplots(figsize=figsize)
     image = ax.imshow(data, origin='lower', cmap=cmap, vmin=vmin, vmax=vmax)
-    ax.plot(meridians[1][:,:-1], meridians[0][:,:-1], color='black', ls='--', lw=0.5, alpha=0.5)
+    ax.plot(meridians[1][:,1:], meridians[0][:,1:], color='black', ls='--', lw=0.5, alpha=0.5)
+    ax.plot(meridians[1][:,0], meridians[0][:,0], color='black', ls='-', lw=0.5, alpha=1) # central meridian
+
     ax.plot(parallels[1][:,:6], parallels[0][:,:6], color='black', ls='--', lw=0.5, alpha=0.5)
     ax.plot(parallels[1][:,6], parallels[0][:,6], color='black', ls='-', lw=0.5, alpha=1) # equator
     ax.plot(parallels[1][:,7:], parallels[0][:,7:], color='black', ls='--', lw=0.5, alpha=0.5)
 
-    for i in range(-180,180,15):
-        ax.annotate(i, (parallels[1][i + 180,6], parallels[0][i + 180,6]), alpha=0.7, size=8)
+    ax.add_patch(Circle((view.yc, view.xc), radius=view.rsun, color='black', ls='-', lw=0.5, alpha=1, fill=False))
+
+    for i in range(0,360,15):
+        ax.annotate(i, (parallels[1][i,6], parallels[0][i,6]), alpha=0.7, size=8)
 
     for i in range(-90,91,15):
         if i != 0:
@@ -70,3 +47,4 @@ def show_data(data, view, title='', label='', figsize=(10,10), cmap='seismic', v
     if to_file is not None:
         plt.savefig(to_file)
         plt.ion()
+        plt.close(fig)
